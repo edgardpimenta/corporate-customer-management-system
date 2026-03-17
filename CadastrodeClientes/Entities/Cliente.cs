a33698c7200d5887e8bf31060ad4621e3649e698
Edgard <edgardpimentaa02@gmail.com>
2026-03-16 23:17:50 -0300

    Adicionar arquivos de projeto.

---
+++ CadastrodeClientes/Entities/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CadastrodeClientes.Entities
+{
+    public class Cliente
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;   
+        public string Telefone { get; set; } = string.Empty;
+        public DateTime DataHoraCadastro { get; set; } = DateTime.Now;
+    }
+}
