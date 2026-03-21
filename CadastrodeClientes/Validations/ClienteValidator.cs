819d8ff7561947b18d5c42a84d76ef170928712e
Edgard <edgardpimentaa02@gmail.com>
2026-03-20 22:50:40 -0300

    feat: add cliente validation logic

---
+++ CadastrodeClientes/Validations/ClienteValidator.cs
@@ -0,0 +1,34 @@
+﻿using CadastrodeClientes.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CadastrodeClientes.Validations
+{
+
+    public class ClienteValidator
+    {
+        public void ValidarCliente(Cliente cliente,string nome, string cpf, string email)
+        {
+
+
+            if (string.IsNullOrWhiteSpace(cpf))
+            {
+                throw new Exception("CPF não pode ser nulo.");
+            }
+
+            if (string.IsNullOrWhiteSpace(nome))
+            {
+                throw new Exception("Nome não pode ser nulo.");
+            }
+
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new Exception("Email não pode ser nulo.");
+            }
+
+        }
+    }
+}
+
