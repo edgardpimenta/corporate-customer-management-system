31152562ea993b8cc4e7298bee648f0a51c975c2
Edgard <edgardpimentaa02@gmail.com>
2026-03-20 22:49:20 -0300

    feat: add empresa entity

---
+++ CadastrodeClientes/Entities/Empresa.cs
@@ -0,0 +1,24 @@
+﻿using CadastrodeClientes.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastrodeClientes.Entities
+{
+    // Modelo de Classe que representa uma empresa
+    public class Empresa
+    {
+        #region Propriedades
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string RazaoSocial { get; set; } = string.Empty;
+        public string Cnpj { get; set; } = string.Empty;
+        public List<Cliente> Clientes { get; set; } = new List<Cliente>(); // Propriedade de navegação para os clientes associados à empresa
+
+        /// <summary>
+        /// Aplicando enum do TipoDeCliente para indicar o tipo de cliente (Pessoa Física ou Pessoa Jurídica) associado à empresa
+        /// </summary>
+        public TipoDeCliente TipoDeCliente { get; set; } = TipoDeCliente.PessoaFisica; // Aplicando o Enum TipoDeCliente para indicar o tipo de cliente (Pessoa Física ou Pessoa Jurídica)
+        #endregion
+
+    }
+}
