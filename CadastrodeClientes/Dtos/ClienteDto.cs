ba5bca4f7ce18354f7c02596b19a21f9ef7a19d5
Edgard <edgardpimentaa02@gmail.com>
2026-04-26 23:28:09 -0300

    feat: add DTOs for cliente operations

---
+++ CadastrodeClientes/Dtos/ClienteDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastrodeClientes.Dtos
+{
+    public class CreateClienteDto
+    { 
+        public string Nome { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+
+
+    public class ClienteDto
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateOnly DataCadastro { get; set; } 
+    }
+}
