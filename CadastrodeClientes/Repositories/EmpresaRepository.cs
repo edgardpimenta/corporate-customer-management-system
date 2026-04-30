9b09985e74baa051653d98969d6623a405fd3849
Edgard <edgardpimentaa02@gmail.com>
2026-04-30 01:43:52 -0300

    ffeat: add Empresa Repository

---
+++ CadastrodeClientes/Repositories/EmpresaRepository.cs
@@ -0,0 +1,37 @@
+﻿using CadastrodeClientes.Entities;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastrodeClientes.Repositories
+{
+    public class EmpresaRepository
+    {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBCadastroClientes;Integrated Security=True;";
+
+        public void CadastrarEmpresa(Empresa empresa)
+        {
+            var query = @"INSERT INTO Empresas (Id,RazaoSocial,Cnpj
+                         VALUES (@Id,@RazaoSocial,@Cnpj)";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                connection.Execute(query, empresa);
+            }
+        }
+
+        public void ListarEmpresa(Guid id)
+        {
+            var query = @"SELECT Empresas
+                          WHERE Id = @Id";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                connection.Execute(query, new {id});
+            }
+        }
+    }
+}
