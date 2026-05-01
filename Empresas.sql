d23f973abfc52c041fe076cdbc2287291025ce3c
Edgard <edgardpimentaa02@gmail.com>
2026-04-30 23:09:58 -0300

    chore: update database scripts, add Empresas.sql and remove obsolete SQLQuery1.sql

---
+++ Empresas.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Empresas] (
+    [Id]   UNIQUEIDENTIFIER DEFAULT (newsequentialid()) NOT NULL,
+    [Nome] NVARCHAR (100)   NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    TipoDeCliente INT NOT NULL,
+    Status INT NOT NULL 
+);
+
